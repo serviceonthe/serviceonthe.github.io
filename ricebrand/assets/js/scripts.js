82cbd96f5ef8ae38755269f9fe907f345d9f4710
serviceonthe <minhaj@serviceontheweb.co.uk>
2017-01-11 22:03:56 +0600

    rice brand up

---
+++ ricebrand/assets/js/scripts.js
@@ -0,0 +1,128 @@
+/*
+Author       : Syed Ekram.
+Template Name: Alvida - One Page Business Template
+Version      : 1.0
+*/
+
+(function($) {
+	'use strict';
+	
+	jQuery(document).on('ready', function(){
+	
+		/*PRELOADER JS*/
+		$(window).on('load', function() { 
+			$('.status').fadeOut();
+			$('.preloader').delay(350).fadeOut('slow'); 
+		}); 
+		/*END PRELOADER JS*/
+
+		/*START MENU JS*/
+			$('a.page-scroll').on('click', function(e){
+				var anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top - 50
+				}, 1500);
+				e.preventDefault();
+			});		
+
+			$(window).on('scroll', function() {
+			  if ($(this).scrollTop() > 100) {
+				$('.menu-top').addClass('menu-shrink');
+			  } else {
+				$('.menu-top').removeClass('menu-shrink');
+			  }
+			});
+			
+			$(document).on('click','.navbar-collapse.in',function(e) {
+			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide');
+			}
+			});				
+		/*END MENU JS*/ 		
+		/*START SLIDER JS*/
+			$('.carousel').carousel({
+				interval:8000,
+				pause:'false',
+			});
+		/*END SLIDER JS*/
+
+		/*START PARTNER LOGO*/
+		$('.partner').owlCarousel({
+		  autoPlay: 3000, //Set AutoPlay to 3 seconds
+		  items : 4,
+		  itemsDesktop : [1199,3],
+		  itemsDesktopSmall : [979,3]
+		});
+		/*END PARTNER LOGO*/		
+
+		/* START COUNTDOWN JS*/
+		$('.counter_feature').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+			if (visible) {
+				$(this).find('.timer').each(function () {
+					var $this = $(this);
+					$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+						duration: 2000,
+						easing: 'swing',
+						step: function () {
+							$this.text(Math.ceil(this.Counter));
+						}
+					});
+				});
+				$(this).unbind('inview');
+			}
+		});
+		/* END COUNTDOWN JS */
+	
+	  /*START PROGRESS BAR*/
+	  $('.progress-bar > span').each(function(){
+			var $this = $(this);
+			var width = $(this).data('percent');
+			$this.css({
+				'transition' : 'width 2s'
+			});
+			
+			setTimeout(function() {
+				$this.appear(function() {
+						$this.css('width', width + '%');
+				});
+			}, 500);
+		});
+		/*END PROGRESS BAR*/
+			
+		/*START GOOGLE MAP*/
+		function initialize() {
+		  var mapOptions = {
+			zoom: 15,
+			scrollwheel: false,
+			center: new google.maps.LatLng(40.7127837, -74.00594130000002)
+		  };
+		  var map = new google.maps.Map(document.getElementById('map'),
+			  mapOptions);
+		  var marker = new google.maps.Marker({
+			position: map.getCenter(),
+			icon: 'assets/img/map_pin.png',
+			map: map
+		  });
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);	
+		/*END GOOGLE MAP*/
+	}); 		
+		
+	/*START MIXITUP JS*/
+		$('.work_all_item').mixItUp();
+		// jQuery Lightbox
+		$('.lightbox').venobox({
+			numeratio: true,
+			infinigall: true
+		});	
+	/*END MIXITUP JS*/
+		
+	/*START WOW ANIMATION JS*/
+	  new WOW().init();	
+	/*END WOW ANIMATION JS*/	
+				
+})(jQuery);
+
+
+  
+
